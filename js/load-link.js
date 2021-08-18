e7fc01264e2b995296c42e144b224b19179cd12a
Yuri-sc <yurisantiagodacosta@gmail.com>
2021-08-18 15:56:18 -0300

    commit homepage

---
+++ js/load-link.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.nav__link').click(function(e) {
+
+        e.preventDefault()
+
+        // Capturando o link
+        let url = $(this).attr('href')
+            // alert('Você clicou')
+
+        // Limpando a div container com o id conteudo
+        $('#content').empty()
+
+        // Inserindo o novo conteúdo dentro de id conteudo
+        $('#content').load(url)
+    })
+})
